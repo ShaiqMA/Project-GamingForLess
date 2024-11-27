4c2ece32fa6139368245a2819a77138692d998c6
Shaiq Ahmed <shaiq.ahmed@student.rocva.nl>
2024-11-27 21:51:35 +0100

    Vandaag heb ik de problemen lopen oplossen die ik had, bijvoorbeeld de mobile menu bar zag ik ook op pc grootte, dat heb ik opgelost door veranderingen in mijn css en html bestand gedaan. mijn volgende stap is om animaties erin te voegen

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 document.querySelector('.hamburger-button').addEventListener('click', function(){
-    const Nav = document.querySelector('.nav-links')
-    if (Nav.style.display === 'none') {
-     Nav.style.display = 'block';
+    const Navm = document.querySelector('.nav-links')
+    if (Navm.style.display === 'none') {
+     Navm.style.display = 'block';
     } else {
-     Nav.style.display = 'none';
+     Navm.style.display = 'none';
     }
  });
