61c4838a900368995ae752d0d5792ee14e3f791d
Shaiq Ahmed <shaiq.ahmed@student.rocva.nl>
2024-11-28 21:51:18 +0100

    ik heb ervoor gezorgd dat de menu een slide in animatie heeft, dat heb ik gedaan door in de css de menu eerst buiten de scherm te zetten, daarna heb ik een .active toegevoeg aan class, daarna heb ik een function in javascript gedaan waardoor de class .active wordt geactiveerd, dit zal er zorgen voor de slidein

--- js/index.js
+++ js/index.js
@@ -4,5 +4,11 @@ document.querySelector('.hamburger-button').addEventListener('click', function()
      Navm.style.display = 'block';
     } else {
      Navm.style.display = 'none';
-    }
- });
+    };
+});
+
+function toggleMenu(){
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active')
+}
+
