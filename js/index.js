16293546fcce8b28e7d67e8cf295815d5e5ed15d
Shaiq Ahmed <shaiq.ahmed@student.rocva.nl>
2024-12-05 18:58:13 +0100

    Ik heb op mijn homepage de sectie popular items toegevoegd. daarin staat de hoofdtext popular items en heb ik vakjes gemaakt waar de producten uiteindelijk komen te staan, ik heb ze responisve gamaakt voor de kleinere devices, daar zijn de vakjes namelijk wat kleiner. ik heb een hover animatie op de vakjes dat ze groter worden. ik heb gemerkt dat de slidein menu een kleinbeetje raar doet op de kleinere schermen, ik zal dit later pas oplossen.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
-document.querySelector('.hamburger-button').addEventListener('click', function(){
-    const Navm = document.querySelector('.nav-links')
-    if (Navm.style.display === 'none') {
-     Navm.style.display = 'block';
-    } else {
-     Navm.style.display = 'none';
-    };
-});
+
+
 
 function toggleMenu(){
     const menu = document.getElementById('menu');
