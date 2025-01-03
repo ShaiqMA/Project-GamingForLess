be7621f8da3c3ee37bde25f20e8cbdcd329c7cde
Shaiq Ahmed <shaiq.ahmed@student.rocva.nl>
2025-01-03 22:57:30 +0100

    Cheapest pagina afgemaakt, afbeeldingen en details moeten nog worden toegevoegd.

--- js/Cheapest.js
+++ js/Cheapest.js
@@ -3,3 +3,7 @@ function toggleMenu(){
     menu.classList.toggle('active')
 };
 
+function toggleFilterMenu(){
+    const menu = document.getElementById('aside');
+    menu.classList.toggle('active')
+};
