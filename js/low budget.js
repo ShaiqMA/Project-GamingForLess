f7c0af92245349e8084ca012fff5637932c282c2
Shaiq Ahmed <shaiq.ahmed@student.rocva.nl>
2025-01-03 23:07:19 +0100

    Low budget items pagina klaar. afbeeldingen en detail zullen nog moeten worden gedaan.

---
+++ js/low budget.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active')
+};
+
+function toggleFilterMenu(){
+    const menu = document.getElementById('aside');
+    menu.classList.toggle('active')
+};
